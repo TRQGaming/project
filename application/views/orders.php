35f281208811d2324149841afdb1c3fc8ffb4e3a
mynameisryan <rymy1515@gmail.com>
2017-02-01 14:52:03 -0800

    readme

--- application/views/orders.php
+++ application/views/orders.php
@@ -256,7 +256,6 @@
                 </tr>
             </table>
         </div>
-        <h1>Troy is the best</h1>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
     	<script src="http://code.jquery.com/ui/1.9.2/jquery-ui.js"></script>
     </body>
