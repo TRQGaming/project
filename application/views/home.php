bb1bc5b548c7eb7dec001e730782d7357857eb5e
mynameisryan <rymy1515@gmail.com>
2017-02-01 21:05:39 -0800

    home

--- application/views/home.php
+++ application/views/home.php
@@ -5,56 +5,155 @@
         <title>Home Page</title>
         <link rel="stylesheet" type="text/css" href="/assets/stylesheets/main.css">
     	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     </head>
     <body>
-        <div class="container col-sm-12 col-lg-12 col-md-12">
-            <div id="myCarousel" class="carousel slide" data-ride="carousel">
-              <!-- Indicators -->
-              <ol class="carousel-indicators">
-                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-                <li data-target="#myCarousel" data-slide-to="1"></li>
-                <li data-target="#myCarousel" data-slide-to="2"></li>
-                <li data-target="#myCarousel" data-slide-to="3"></li>
-              </ol>
-
-              <!-- Wrapper for slides -->
-              <div class="carousel-inner" role="listbox">
-                <div class="item active">
-                  <img src="https://s3-us-west-1.amazonaws.com/shacknews/assets/editorial/2016/04/Enter-the-Gungeon-Review-10.jpg" alt="Chania">
-                  <div class="carousel-caption">
+        <div class="black">
+            <div class="col-sm-12 col-lg-12 col-md-12">
+                <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                  <ol class="carousel-indicators">
+                    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                    <li data-target="#myCarousel" data-slide-to="1"></li>
+                    <li data-target="#myCarousel" data-slide-to="2"></li>
+                    <li data-target="#myCarousel" data-slide-to="3"></li>
+                  </ol>
+                  <div class="carousel-inner" role="listbox">
+                    <div class="item active">
+                      <img src="http://embed.wistia.com/deliveries/c9d25d3790f1f7138a6ff840525490647caed14d.jpg" alt="Chania">
+                      <div class="carousel-caption">
+                          <div class="jumbotron middlemiddle" id="homem" >
+                            <h2 id="login">Game  <span class="glyphicon glyphicon-cloud"></span></h2>
+                            <div class="middle room">
+                                <a class="middle" href="games"><h3 id="login">View  <span class="glyphicon glyphicon-off"></span></h3></a>
+                                <a class="middle" href="index"><h5 id="login">Login  <span class="glyphicon glyphicon-user"></span></h5></a>
+                            </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="item">
+                      <img src="http://static.giantbomb.com/uploads/original/2/29381/2830173-stardewvalley.png" alt="Chania">
+                      <div class="carousel-caption">
+                          <div class="jumbotron middlemiddle" id="homem" >
+                            <h2 id="login">Game  <span class="glyphicon glyphicon-cloud"></span></h2>
+                            <div class="middle room">
+                                <a class="middle" href="games"><h3 id="login">View  <span class="glyphicon glyphicon-off"></span></h3></a>
+                                <a class="middle" href="index"><h5 id="login">Login  <span class="glyphicon glyphicon-user"></span></h5></a>
+                            </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="item">
+                      <img src="http://assets1.ignimgs.com/2013/10/18/stanley-parablejpg-8845f3_1280w.jpg" alt="Flower">
+                      <div class="carousel-caption">
+                          <div class="jumbotron middlemiddle" id="homem" >
+                            <h2 id="login">Game  <span class="glyphicon glyphicon-cloud"></span></h2>
+                            <div class="middle room">
+                                <a class="middle" href="games"><h3 id="login">View  <span class="glyphicon glyphicon-off"></span></h3></a>
+                                <a class="middle" href="index"><h5 id="login">Login  <span class="glyphicon glyphicon-user"></span></h5></a>
+                            </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="item">
+                      <img src="https://s.aolcdn.com/hss/storage/midas/f31af0b298713b84e64f7c1397e4f6ea/201739749/HotlineMiami2.jpg" alt="Flower">
+                      <div class="carousel-caption">
+                          <div class="jumbotron middlemiddle" id="homem" >
+                            <h2 id="login">Game  <span class="glyphicon glyphicon-cloud"></span></h2>
+                            <div class="middle room">
+                                <a class="middle" href="games"><h3 id="login">View  <span class="glyphicon glyphicon-off"></span></h3></a>
+                                <a class="middle" href="index"><h5 id="login">Login  <span class="glyphicon glyphicon-user"></span></h5></a>
+                            </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="item">
+                      <img src="http://cdn.idigitaltimes.com/sites/idigitaltimes.com/files/2016/12/25/night-woods-screenshot.jpg" alt="Flower">
+                      <div class="carousel-caption">
+                          <div class="jumbotron middlemiddle" id="homem" >
+                            <h2 id="login">Game  <span class="glyphicon glyphicon-cloud"></span></h2>
+                            <div class="middle room">
+                                <a class="middle" href="games"><h3 id="login">View  <span class="glyphicon glyphicon-off"></span></h3></a>
+                                <a class="middle" href="index"><h5 id="login">Login  <span class="glyphicon glyphicon-user"></span></h5></a>
+                            </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="item">
+                      <img src="http://playstarbound.com/wp-content/uploads/2016/07/sb_releasedate_facebook.png" alt="Flower">
+                      <div class="carousel-caption">
+                          <div class="jumbotron middlemiddle" id="homem" >
+                            <h2 id="login">Game  <span class="glyphicon glyphicon-cloud"></span></h2>
+                            <div class="middle room">
+                                <a class="middle" href="games"><h3 id="login">View  <span class="glyphicon glyphicon-off"></span></h3></a>
+                                <a class="middle" href="index"><h5 id="login">Login  <span class="glyphicon glyphicon-user"></span></h5></a>
+                            </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="item">
+                      <img src="https://cdn.shopify.com/s/files/1/0014/1962/products/ss_8bc4c5c483fb85bcdcbcdbe98cbe44e82bfc961f.1920x1080.jpg?v=1479428977" alt="Flower">
+                      <div class="carousel-caption">
+                          <div class="jumbotron middlemiddle" id="homem" >
+                            <h2 id="login">Game  <span class="glyphicon glyphicon-cloud"></span></h2>
+                            <div class="middle room">
+                                <a class="middle" href="games"><h3 id="login">View  <span class="glyphicon glyphicon-off"></span></h3></a>
+                                <a class="middle" href="index"><h5 id="login">Login  <span class="glyphicon glyphicon-user"></span></h5></a>
+                            </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="item">
+                      <img src="https://www.windowscentral.com/sites/wpcentral.com/files/styles/larger/public/field/image/2016/08/starbound-3.jpg?itok=dqyFHVXM" alt="Flower">
+                      <div class="carousel-caption">
+                          <div class="jumbotron middlemiddle" id="homem" >
+                            <h2 id="login">Game  <span class="glyphicon glyphicon-cloud"></span></h2>
+                            <div class="middle room">
+                                <a class="middle" href="games"><h3 id="login">View  <span class="glyphicon glyphicon-off"></span></h3></a>
+                                <a class="middle" href="index"><h5 id="login">Login  <span class="glyphicon glyphicon-user"></span></h5></a>
+                            </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="item">
+                      <img src="https://s-media-cache-ak0.pinimg.com/originals/73/cc/21/73cc21743b281769693c01775815951e.jpg" alt="Flower">
+                      <div class="carousel-caption">
+                          <div class="jumbotron middlemiddle" id="homem" >
+                            <h2 id="login">Game  <span class="glyphicon glyphicon-cloud"></span></h2>
+                            <div class="middle room">
+                                <a class="middle" href="games"><h3 id="login">View  <span class="glyphicon glyphicon-off"></span></h3></a>
+                                <a class="middle" href="index"><h5 id="login">Login  <span class="glyphicon glyphicon-user"></span></h5></a>
+                            </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="item">
+                      <img src="http://static1.gamespot.com/uploads/screen_kubrick/1537/15377506/2808123-gsplays_sunlesssea_20150212.jpg" alt="Flower">
+                      <div class="carousel-caption">
+                          <div class="jumbotron middlemiddle" id="homem" >
+                            <h2 id="login">Game  <span class="glyphicon glyphicon-cloud"></span></h2>
+                            <div class="middle room">
+                                <a class="middle" href="games"><h3 id="login">View  <span class="glyphicon glyphicon-off"></span></h3></a>
+                                <a class="middle" href="index"><h5 id="login">Login  <span class="glyphicon glyphicon-user"></span></h5></a>
+                            </div>
+                        </div>
+                      </div>
+                    </div>
                   </div>
-                </div>
-
-                <div class="item">
-                  <img src="http://stardewvalley.net/wp-content/uploads/2016/09/1_1_screenshot3_7.png" alt="Chania">
-                  <div class="carousel-caption">
-                  </div>
-                </div>
-
-                <div class="item">
-                  <img src="http://assets1.ignimgs.com/2013/10/18/stanley-parablejpg-8845f3_1280w.jpg" alt="Flower">
-                  <div class="carousel-caption">
-                  </div>
-                </div>
-
-                <div class="item">
-                  <img src="https://s.aolcdn.com/hss/storage/midas/f31af0b298713b84e64f7c1397e4f6ea/201739749/HotlineMiami2.jpg" alt="Flower">
-                  <div class="carousel-caption">
-                  </div>
-                </div>
+                  <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                  </a>
+                  <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                  </a>
               </div>
-
-              <!-- Left and right controls -->
-              <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-              </a>
-              <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-              </a>
-          </div>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-    	<script src="http://code.jquery.com/ui/1.9.2/jquery-ui.js"></script>
+              <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+              <script src="http://code.jquery.com/ui/1.9.2/jquery-ui.js"></script>
+            </div>
+        </div>
     </body>
 </html>
